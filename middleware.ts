9c50812724e685bcd61a39d7c6a9007d92decc53
awais512 <raza.awais25@gmail.com>
2024-06-24 13:19:26 +0100

    Organiazation setup and switching between different orgs functionality with clerk

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,28 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(["/"]);
+// // const isProtectedRoute = createRouteMatcher(["/"]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) {
-    auth().protect();
-  }
-  return NextResponse.next();
-});
+// const isProtectedRoute = createRouteMatcher(["/(.*)"]);
+
+// export default clerkMiddleware((auth, req) => {
+//   if (isProtectedRoute(req)) {
+//     auth().protect();
+//   }
+//   return NextResponse.next();
+// });
+
+// export const config = {
+//   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+import { authMiddleware } from "@clerk/nextjs";
+
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+export default authMiddleware({});
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
