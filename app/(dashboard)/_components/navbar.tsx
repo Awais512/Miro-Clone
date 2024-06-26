144168d61618ea7725bf2e33ed88e8717404c08b
awais512 <raza.awais25@gmail.com>
2024-06-26 23:04:01 +0100

    Search Input

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
+import SearchInput from "./search-input";
 
 export const Navbar = () => {
   return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-500">
-      <div className="hidden lg:flex-1 lg:flex bg-yellow-500">Search</div>
+    <div className="flex items-center gap-x-4 p-5">
+      <div className="hidden lg:flex-1 lg:flex">
+        <SearchInput />
+      </div>
       <UserButton />
     </div>
   );
