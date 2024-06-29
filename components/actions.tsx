436f779f090961d3ec679c79860536c0a00ebd77
awais512 <raza.awais25@gmail.com>
2024-06-29 14:45:47 +0100

    Resuable modal using shadcn-ui

--- components/actions.tsx
+++ components/actions.tsx
@@ -11,6 +11,8 @@ import { Link2, Trash2 } from "lucide-react";
 import { toast } from "sonner";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 import { api } from "@/convex/_generated/api";
+import { ConfirmModal } from "./confirm-modal";
+import { Button } from "./ui/button";
 
 interface Props {
   children: React.ReactNode;
@@ -53,14 +55,22 @@ export const Actions = ({ children, side, sideOffset, id, title }: Props) => {
           <Link2 className="size-4 mr-2" />
           Copy board link
         </DropdownMenuItem>
-        <DropdownMenuItem
+        <ConfirmModal
+          header="Delete Board?"
+          description="This will delete the board and all of its content"
           disabled={pending}
-          onClick={onDelete}
-          className="p-3 cursor-pointer"
+          onConfirm={onDelete}
         >
-          <Trash2 className="size-4 mr-2" />
-          Delete
-        </DropdownMenuItem>
+          <Button
+            variant="ghost"
+            disabled={pending}
+            // onClick={onDelete}
+            className="p-3 cursor-pointer text-sm w-full justify-start font-normal"
+          >
+            <Trash2 className="size-4 mr-2" />
+            Delete
+          </Button>
+        </ConfirmModal>
       </DropdownMenuContent>
     </DropdownMenu>
   );
