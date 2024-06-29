00c3289df7af3a89269482b276faa5d802caec21
awais512 <raza.awais25@gmail.com>
2024-06-30 00:33:24 +0100

    Search and favorite boards

--- convex/board.ts
+++ convex/board.ts
@@ -45,6 +45,18 @@ export const remove = mutation({
     if (!identity) {
       throw new Error("Unauthorized");
     }
+    const userId = identity.subject;
+    const existingFavorite = await ctx.db
+      .query("userFavorites")
+      .withIndex("by_user_board", (q) =>
+        q.eq("userId", userId).eq("boardId", args.id)
+      )
+      .unique();
+
+    if (existingFavorite) {
+      await ctx.db.delete(existingFavorite._id);
+    }
+
     await ctx.db.delete(args.id);
   },
 });
@@ -84,8 +96,8 @@ export const favorite = mutation({
     const userId = identity.subject;
     const existingFavorites = await ctx.db
       .query("userFavorites")
-      .withIndex("by_user_board_org", (q) =>
-        q.eq("userId", userId).eq("boardId", board._id).eq("orgId", args.orgId)
+      .withIndex("by_user_board", (q) =>
+        q.eq("userId", userId).eq("boardId", board._id)
       )
       .unique();
     if (existingFavorites) {
